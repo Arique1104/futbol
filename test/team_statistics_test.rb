d8ee435133f4c0457c34638e394f632004bbeaf5
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-29 11:19:12 -0600

    :white_check_mark: Add first tests and comment in thoughts

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -24,4 +24,41 @@ class TeamStatisticsTest < MiniTest::Test
     assert_equal './team_stats_fixtures/teams_fixtures.csv', @stat_tracker.teams
     assert_equal './team_stats_fixtures/game_teams_fixtures.csv', @stat_tracker.game_teams
   end
+
+
+  def test_can_find_home_or_away_team_id
+    assert_equal true, @stat_tracker.home_or_away_team_id(6)
+  end
+
+  def test_it_can_find_season_home_team_losses
+    assert_equal 0, @stat_tracker.season_home_team_losses(6)
+  end
+
+# out of 14 games for team id 6
+# (the id flops back and forth between home id and away id)
+# 2012 2013
+# post season away wins: 4
+# post season away losses:
+# post season away ties:
+# post season home wins: 5
+# post season home losses:
+# post season home ties:
+
+# = 9 wins 0 losses 0 ties post season
+
+# 2013 2014
+# regular season away wins:
+# regular season away losses:
+# regular season away ties: 1
+# regular season home wins: 4
+# regular season home losses:
+# regular season home ties:
+
+# = 4 wins 0 losses 1 tie regular season
+
+# Ask about ties and how they factor in. I know the above who clearly
+# indicate that the post season was the best win % season, however over the rest of the data
+# we need to know how ties factor in as they could dramatically change the data.
+
+
 end
