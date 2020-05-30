df1211898387e40189246eaa1d157506215d647a
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-30 14:38:02 -0600

    :construction: Work on most accurate team

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -3,11 +3,87 @@ require "csv"
 
 class SeasonStatistics < StatTracker
 
-  def games_by_season(season)
-    all_rows = CSV.read(@games, :headers=>true, :header_converters=>:symbol)
-    all_rows.select do |row|
-      row[:season] == season
+  #  seasons are in games.csv
+  # goals and shots are in game_teams.csv
+  # the return value is a string that represents the team name
+  # given any season id
+  # calculate goals divided by shots
+
+  # def team_name(id)
+  #   rows = CSV.read(@teams, :headers => true, :header_converters => :symbol)
+  #   rows.find do |row|
+  #     return row[:teamname] if row[:team_id] == id.to_s
+  #   end
+  # end
+
+
+
+
+  def most_accurate_team(season)
+
+    games_rows = CSV.read(@games, :headers => true, :header_converters => :symbol)
+
+    #in regular season
+    game_ids_in_season = games_rows.reduce([]) do |acc, row|
+      acc << row[:game_id] if row[:season] == season #&& row[:type] == "Regular Season"
+      acc
     end
+
+
+
+
+
+    game_teams_rows = CSV.read(@game_teams, :headers => true, :header_converters => :symbol)
+
+    games_played_in_season = game_teams_rows.find_all do |row|
+       game_ids_in_season.include?(row[:game_id])
+    end
+
+    # name of the team with the best ratio of shots to goals in a season
+    # find total goals and shots by a team in season
+
+    # build hash
+    # keys are team id
+    # values are collection of goals and shots
+    # calculate goals and shots in %
+    # get team_id with highest %
+    # row[:team_id]
+    # row[:goals]
+    # row[:shots]
+    # hash = {row[:team_id] => [0, 0]}
+    # hash = {row[:team_id] => [row[:goals], row[:shots]]}
+    # hash = {row[:team_id] => accuracy during season}
+    # try helper method for accuracy during season
+
+
+    # every key is the team_id
+    # every value is their average
+
+    games_by_team = games_played_in_season.group_by do |row|
+      row[:team_id]
+    end
+
+    games_by_team.each do |team, rows|
+      rows = rows.reduce([0, 0]) do |acc, row|
+        acc[0] += row[:goals].to_f
+        acc[1] += row[:shots].to_f
+        acc
+      end
+    end
+    require "pry"; binding.pry
+
+
+
+
+    # acc[row[:team_id]][0] += row[:goals].to_f
+    # acc[row[:team_id]][1] += row[:shots].to_f
+
+
+    # team_rows = CSV.read(@teams, :headers => true, :header_converters => :symbol)
+    #
+    # team_name = team_rows.find do | row|
+    #   row[:team_id] == most_accurate_team_id
+    # end[:teamname]
+
   end
-  
 end
--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -5,9 +5,9 @@ require "minitest/pride"
 class SeasonStatisticsTest < MiniTest::Test
 
   def setup
-    # game_path =
-    # team_path =
-    # game_teams_path =
+    game_path = "./data/games.csv"
+    team_path = "./data/teams.csv"
+    game_teams_path = "./data/game_teams.csv"
 
     file_path_locations = {
       games: game_path,
@@ -20,12 +20,14 @@ class SeasonStatisticsTest < MiniTest::Test
 
   # Name of the Coach with the best win percentage for the season
   def test_it_gets_winningest_coach
+    skip
     assert_equal "Claude Julien", @stat_tracker.winningest_coach("20132014")
     assert_equal "Alain Vigneault", @stat_tracker.winningest_coach("20142015")
   end
 
   # Name of the Coach with the worst win percentage for the season
   def test_it_gets_worst_coach
+    skip
     assert_equal "Peter Laviolette", @stat_tracker.worst_coach("20132014")
     # assert_equal("Craig MacTavish").or(eq("Ted Nolan")), @stat_tracker.worst_coach("20142015")
   end
@@ -38,18 +40,21 @@ class SeasonStatisticsTest < MiniTest::Test
 
   # Name of the Team with the worst ratio of shots to goals for the season
   def test_it_gets_least_accurate_team
+    skip
     assert_equal "New York City FC", @stat_tracker.least_accurate_team("20132014")
     assert_equal "Columbus Crew SC", @stat_tracker.least_accurate_team("20142015")
   end
 
   # Name of the Team with the most tackles in the season
   def test_it_gets_most_tackles
+    skip
     assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20132014")
     assert_equal "Seattle Sounders FC", @stat_tracker.most_tackles("20142015")
   end
 
   # Name of the Team with the fewest tackles in the season
   def test_it_gets_fewest_tackles
+    skip
     assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
     assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
   end
