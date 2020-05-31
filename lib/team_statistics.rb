7d7c345022dfff8f35ad068c79c067f6d42d7409
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-31 16:49:09 -0600

    :construction: find all games in a season for best season

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -12,5 +12,10 @@ module TeamStatistics
     team_seasons = @games.find_all do |game|
       game.home_team_id  == team_id || game.away_team_id == team_id
     end
+
+    games_in_seasons = team_seasons.group_by do |game|
+      game.season
+    end
+    require "pry"; binding.pry
   end
 end
