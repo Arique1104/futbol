64bd51ee0048fb55cadb9a6fe8d31474aa9d1f72
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-31 17:28:45 -0600

    :sparkles: Complete worst season method

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -34,4 +34,28 @@ module TeamStatistics
      win_percentage
     end[0]
   end
+
+  def worst_season(team_id)
+    team_seasons = @games.find_all do |game|
+      game.home_team_id  == team_id || game.away_team_id == team_id
+    end
+
+    games_in_season = team_seasons.group_by do |game|
+      game.season
+    end
+
+    games_in_season.each do |season, season_games|
+    season_game_ids = season_games.map do |game|
+      game.game_id
+      end
+
+      team_games = @game_teams.find_all do |game|
+        game.team_id == team_id && season_game_ids.include?(game.game_id)
+      end
+      games_in_season[season] = win_percentage(team_games)
+    end
+    games_in_season.min_by do |season, win_percentage| #find the season with the worst win percentage
+     win_percentage
+    end[0]
+  end
 end
