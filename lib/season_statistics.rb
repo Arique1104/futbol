44eec6fd128cbf65c25ee8e717f470f8c28f4c0c
Arique Aguilar <arique1104@gmail.com>
2020-05-31 20:15:15 -0500

    Winningest coach mpassing code

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -1,13 +1,27 @@
-require_relative "./stat_tracker"
-require "csv"
+module SeasonStatistics
 
-class SeasonStatistics < StatTracker
 
-  def games_by_season(season)
-    all_rows = CSV.read(@games, :headers=>true, :header_converters=>:symbol)
-    all_rows.select do |row|
-      row[:season] == season
+  def winningest_coach(season)
+
+    game_ids_in_season =  @games.reduce([]) do |acc, game|
+      if game.season == season
+        acc << game.game_id
+      end
+      acc
     end
+
+    results_by_head_coach = @game_teams.reduce(Hash.new {|hash, key| hash[key] = []}) do |acc, game_team|
+      if game_ids_in_season.include?(game_team.game_id)
+        acc[game_team.head_coach] << game_team.result
+      end
+      acc
+    end
+
+    winningest_coach = results_by_head_coach.max_by do |coach, results|
+      results_by_head_coach[coach] = (results.count {|x| x == "WIN"}) / results.count.to_f
+    end.first
+
   end
-  
+
+
 end
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,7 +3,18 @@ require_relative "./teams_collection"
 require_relative "./games_collection"
 require_relative "./game_teams_collection"
 
+# require_relative "./game_statistics"
+# require_relative "./league_statistics"
+require_relative "./season_statistics"
+# require_relative "./team_statistics"
+
 class StatTracker
+
+  # include GameStatistics
+  # include LeagueStatistics
+  include SeasonStatistics
+  # include TeamStatistics
+
   attr_reader :games,
               :teams,
               :game_teams
@@ -35,5 +46,7 @@ class StatTracker
     games.load_csv
     games.collection
   end
+
+  # Season Statistics <NAME OF METHOD> HELPER? = true || false
 
 end
