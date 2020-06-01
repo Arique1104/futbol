b57cf6defcfd08e8df8441d18960604a0fe8c39e
Arique Aguilar <arique1104@gmail.com>
2020-05-31 22:12:16 -0500

    Refactor and create results by head coach hash helper method

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -2,33 +2,32 @@ module SeasonStatistics
 
 
   def winningest_coach(season)
-    game_ids = game_ids_in_season(season)
-    results_by_head_coach = @game_teams.reduce(Hash.new {|hash, key| hash[key] = []}) do |acc, game_team|
-      acc[game_team.head_coach] << game_team.result if game_ids.include?(game_team.game_id.to_i)
-      acc
-    end
-
-    winningest_coach = results_by_head_coach.max_by do |coach, results|
-      results_by_head_coach[coach] = (results.count {|x| x == "WIN"}) / results.count.to_f
+    coach_results = results_by_head_coach(season)
+    winningest_coach = coach_results.max_by do |coach, results|
+      coach_results[coach] = (results.count {|x| x == "WIN"}) / results.count.to_f
     end.first
-
   end
 
   def worst_coach(season)
-    game_ids = game_ids_in_season(season)
-    results_by_head_coach = @game_teams.reduce(Hash.new {|hash, key| hash[key] = []}) do |acc, game_team|
-      acc[game_team.head_coach] << game_team.result if game_ids.include?(game_team.game_id.to_i)
-      acc
-    end
-
-    worst_coach = results_by_head_coach.min_by do |coach, results|
-      results_by_head_coach[coach] = (results.count {|x| x == "WIN"}) / results.count.to_f
+    coach_results = results_by_head_coach(season)
+    worst_coach = coach_results.min_by do |coach, results|
+      coach_results[coach] = (results.count {|x| x == "WIN"}) / results.count.to_f
     end.first
   end
 
   def game_ids_in_season(season)
     @games.reduce([]) do |acc, game|
       acc << game.game_id.to_i if game.season == season
+      acc
+    end
+  end
+
+  def results_by_head_coach(season)
+    game_ids = game_ids_in_season(season)
+    @game_teams.reduce(Hash.new {|hash, key| hash[key] = []}) do |acc, game_team|
+      if game_ids.include?(game_team.game_id.to_i)
+        acc[game_team.head_coach] << game_team.result
+      end
       acc
     end
   end
