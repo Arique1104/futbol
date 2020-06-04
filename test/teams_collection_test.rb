1febda8561da8510a361fa12f8a33aac13f290d7
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-04 06:31:19 -0600

    Add test for teams_collection while inheriting from superclass

--- test/teams_collection_test.rb
+++ test/teams_collection_test.rb
@@ -7,20 +7,18 @@ require "csv"
 class TeamsCollectionTest < MiniTest::Test
 
 	def setup
-    teams_csv_location = "./data/teams.csv"
-    @teams_collection = TeamsCollection.new(teams_csv_location)
+    @teams_csv_location = "./data/teams.csv"
+    @teams_collection = TeamsCollection.new
 	end
 
-	def test_it_exists
+	def test_it_exists_with_attributes
 		assert_instance_of TeamsCollection, @teams_collection
+		assert_equal Teams, @teams_collection.statistics
+		assert_equal [], @teams_collection.collection
 	end
-
-  def test_it_starts_empty_collection
-    assert_equal [], @teams_collection.collection
-  end
 
   def test_it_can_load_csv
-    @teams_collection.load_csv
+    @teams_collection.load_csv(@teams_csv_location)
     assert_equal 32, @teams_collection.collection.count
     all_teams_object = @teams_collection.collection.all? do |object|
       object.is_a?(Teams)
