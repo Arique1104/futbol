0e9908a923732cc82ffeb2f089efac1ba5fef306
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-04 06:40:26 -0600

    Update game teams collection test to use inheritance pattern

--- test/game_teams_collection_test.rb
+++ test/game_teams_collection_test.rb
@@ -6,21 +6,18 @@ require "./lib/game_teams_collection"
 class GameTeamsCollectionTest < Minitest::Test
 
   def setup
-    game_teams_csv_location = './data/game_teams.csv'
-    @game_teams_collection = GameTeamsCollection.new(game_teams_csv_location)
-
+    @game_teams_csv_location = './data/game_teams.csv'
+    @game_teams_collection = GameTeamsCollection.new
   end
 
   def test_it_exists
     assert_instance_of GameTeamsCollection, @game_teams_collection
-  end
-
-  def test_it_has_attributes
+    assert_equal GameTeams, @game_teams_collection.statistics
     assert_equal [], @game_teams_collection.collection
   end
 
   def test_it_can_load_a_csv_file
-    @game_teams_collection.load_csv
+    @game_teams_collection.load_csv(@game_teams_csv_location)
     assert_equal 14882, @game_teams_collection.collection.count
   end
 
