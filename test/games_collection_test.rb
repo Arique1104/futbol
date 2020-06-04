0572de27701ee21a564ea7b894b545afcc9b1610
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-04 06:37:06 -0600

    Update games collection test to use new pattern

--- test/games_collection_test.rb
+++ test/games_collection_test.rb
@@ -2,25 +2,22 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/games_collection"
 require "./lib/games"
-require "csv"
 
 class GamesCollectionTest < MiniTest::Test
 
 	def setup
-    games_csv_location = "./data/games.csv"
-    @games_collection = GamesCollection.new(games_csv_location)
+    @games_csv_location = "./data/games.csv"
+    @games_collection = GamesCollection.new
 	end
 
-	def test_it_exists
+	def test_it_exists_with_attributes
 		assert_instance_of GamesCollection, @games_collection
+		assert_equal Games, @games_collection.statistics
+		assert_equal [], @games_collection.collection
 	end
-
-  def test_it_starts_empty_collection
-    assert_equal [], @games_collection.collection
-  end
 
   def test_it_can_load_csv
-    @games_collection.load_csv
+    @games_collection.load_csv(@games_csv_location)
     assert_equal 7441, @games_collection.collection.count
     all_games_object = @games_collection.collection.all? do |object|
       object.is_a?(Games)
